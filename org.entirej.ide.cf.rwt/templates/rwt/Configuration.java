10d2bcc015ce7c6cb7e7cd35b68ea6487ff8a358
Anuradha Gunasekara <theanuradha@gmail.com>
2014-02-15 00:06:24 +0530

    latest Tabris cf jars and template updates

--- org.entirej.ide.cf.rwt/templates/rwt/Configuration.java
+++ org.entirej.ide.cf.rwt/templates/rwt/Configuration.java
@@ -10,15 +10,7 @@ import com.eclipsesource.tabris.ui.UIConfiguration;
 public class ApplicationLauncher extends EJTMTApplicationLauncher
 {
 
-    @Override
-    protected void initRootPageConfiguration(UIConfiguration configuration)
-    {
-        //OVERRIDE to add default root pages - by default CF will add Menu page
-        addRootPageConfiguration(configuration, DefaultMenuPage.ID, DefaultMenuPage.class, "Menu");
-        
-        //adding Form as a Root Page
-        //addRootFormComponentPage(configuration, ARootFormPage.PAGE_ID,ARootFormPage.FORM_ID, ARootFormPage.class, "AForm");
-    }
+   
     
     @Override
     public void configure(Application configuration)
@@ -38,17 +30,21 @@ public class ApplicationLauncher extends EJTMTApplicationLauncher
         return "Loading EntireJ Tabris Application (Mobile)";
     }
     
-    
-    public static class ARootFormPage extends EJTMTFormComponentPage
+    /*uncomment to provide default Authenticate support
+    @Override
+    public EJTMTAuthenticateProvider getAuthenticateProvider(EJFrameworkHelper frameworkHelper)
     {
-        static final String PAGE_ID = "A_FORM_ID_ROOT_PAGE";// change A_FORM_ID to EJ Form Name
-        static final String FORM_ID = "A_FORM_ID";// change A_FORM_ID to EJ Form Name
-
-        
-        public ARootFormPage()
+        return new EJTMTDefaultAuthenticateProvider()
         {
-            super(FORM_ID);
-        }
-        
+            
+            @Override
+            public String authenticate(EJFrameworkHelper frameworkHelper, String user, String password)
+            {
+                //return validation error or null signin
+                return null;
+            }
+        };
     }
+    */
+    
 }
