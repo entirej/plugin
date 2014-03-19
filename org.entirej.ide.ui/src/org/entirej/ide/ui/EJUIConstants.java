4788a9c43d710d3c9b20496684bb0f150802a4fe
Anuradha Gunasekara <theanuradha@gmail.com>
2014-03-19 21:52:58 +0530

    EJPLUGIN-169: make sure to pass correct wizard id’s

--- org.entirej.ide.ui/src/org/entirej/ide/ui/EJUIConstants.java
+++ org.entirej.ide.ui/src/org/entirej/ide/ui/EJUIConstants.java
@@ -21,17 +21,17 @@ package org.entirej.ide.ui;
 public class EJUIConstants
 {
 
-    public static final String EJ_PROJECT_WIZARD_ID        = "org.entirej.ide.ui.wizards.NewEntireJProjectWizard";                    //$NON-NLS-1$
-    public static final String EJ_ACTION_SET_ID            = "org.entirej.ide.ui.ActionSet";                                          //$NON-NLS-1$
+    public static final String EJ_PROJECT_WIZARD_ID        = "org.entirej.ide.ui.wizards.NewEntireJProjectWizard";    //$NON-NLS-1$
+    public static final String EJ_ACTION_SET_ID            = "org.entirej.ide.ui.ActionSet";                          //$NON-NLS-1$
 
-    public static final String EJ_FORM_EDITOR_ID           = "org.entirej.ide.ui.editors.ej.properties";                              //$NON-NLS-1$
-    public static final String EJ_FORM_REF_LOV_EDITOR_ID   = "org.entirej.ide.ui.editors.ej.lov";                                     //$NON-NLS-1$
-    public static final String EJ_FORM_REF_BLOCK_EDITOR_ID = "org.entirej.ide.ui.editors.ej.block";                                   //$NON-NLS-1$
-    public static final String EJ_FORM_WIZARD_ID           = "org.entirej.ide.ui.wizards.NewEntireJFormWizard";                       //$NON-NLS-1$
-    public static final String EJ_SERVICE_WIZARD_ID        = "org.entirej.ide.ui.wizards.NewEJPojoServiceWizard";                     //$NON-NLS-1$
-    public static final String EJ_REUSABLEBLOCK_WIZARD_ID  = "org.entirej.framework.plugin.ui.wizards.NewReusableBlockWizard";        //$NON-NLS-1$
-    public static final String EJ_REUSABLELOVDEF_WIZARD_ID = "org.entirej.framework.plugin.ui.wizards.NewReusableLovDefinitionWizard"; //$NON-NLS-1$
-    public static final String EJ_REUSABLEOBJECT_GROUP     = "org.entirej.ide.ui.wizards.form.NewEntireJObjectGroupWizard";           //$NON-NLS-1$
+    public static final String EJ_FORM_EDITOR_ID           = "org.entirej.ide.ui.editors.ej.properties";              //$NON-NLS-1$
+    public static final String EJ_FORM_REF_LOV_EDITOR_ID   = "org.entirej.ide.ui.editors.ej.lov";                     //$NON-NLS-1$
+    public static final String EJ_FORM_REF_BLOCK_EDITOR_ID = "org.entirej.ide.ui.editors.ej.block";                   //$NON-NLS-1$
+    public static final String EJ_FORM_WIZARD_ID           = "org.entirej.ide.ui.wizards.NewEntireJFormWizard";       //$NON-NLS-1$
+    public static final String EJ_SERVICE_WIZARD_ID        = "org.entirej.ide.ui.wizards.NewEJPojoServiceWizard";     //$NON-NLS-1$
+    public static final String EJ_REUSABLEBLOCK_WIZARD_ID  = "org.entirej.ide.ui.wizards.NewEntireJRefBlockWizard";   //$NON-NLS-1$
+    public static final String EJ_REUSABLELOVDEF_WIZARD_ID = "org.entirej.ide.ui.wizards.NewEntireJRefLovWizard";     //$NON-NLS-1$
+    public static final String EJ_REUSABLEOBJECT_GROUP     = "org.entirej.ide.ui.wizards.NewEntireJObjectGroupWizard"; //$NON-NLS-1$
 
     private EJUIConstants()
     {
