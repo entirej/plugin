0dc135e087269bd7efc44edea451a84d4317814a
Anuradha Gunasekara <theanuradha@gmail.com>
2015-06-05 12:43:07 +0530

    Revert "EJPLUGIN-204: drop RCP CF support and merge rap project to main project"
    This reverts commit 929673ab5be147dd426b04eadfe8544dc4d26af6.

---
+++ org.entirej.ide.cf.rwt/templates/rwt/RCPApplication.java
@@ -0,0 +1,12 @@
+package org.entirej;
+
+import org.entirej.applicationframework.rwt.application.launcher.EJRWTRCPApplication;
+
+public class RCPApplication extends EJRWTRCPApplication
+{
+    public static void main(String[] args)
+    {
+        RCPApplication application = new RCPApplication();
+        application.run();
+    }
+}
