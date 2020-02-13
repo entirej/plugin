d14b65856918ae91784c57987ae230ffa6014c8e
Anuradha Gunasekara <theanuradha@gmail.com>
2020-02-13 11:24:13 +0530

    support file blob

--- org.entirej.ora.gen/src/main/java/org/entirej/EJStatementParameterBlob.java
+++ org.entirej.ora.gen/src/main/java/org/entirej/EJStatementParameterBlob.java
@@ -1,33 +1,68 @@
 package org.entirej;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.sql.Types;
 
+import org.entirej.framework.core.EJApplicationException;
 import org.entirej.framework.core.service.EJParameterType;
 import org.entirej.framework.core.service.EJStatementParameter;
 
+public class EJStatementParameterBlob extends EJStatementParameter {
+	private static final int JDBC_TYPE = Types.BLOB;
 
-public class EJStatementParameterBlob extends EJStatementParameter
-{
-    private static final int JDBC_TYPE = Types.BLOB;
+	public EJStatementParameterBlob(EJParameterType type) {
+		super(type);
+	}
 
-    public EJStatementParameterBlob(EJParameterType type)
-    {
-        this(type, null);
-    }
+	public EJStatementParameterBlob(EJParameterType type, byte[] value) {
+		super(type);
+		setValue(value);
+	}
 
-    public EJStatementParameterBlob(EJParameterType type, byte[] value)
-    {
-        super(type);
-        setValue(value);
-    }
+	public EJStatementParameterBlob(EJParameterType type, File value) {
+		super(type);
+		setValue(value);
+	}
 
-    
+	public int getJdbcType() {
+		return JDBC_TYPE;
+	}
 
-    public int getJdbcType()
-    {
-        return JDBC_TYPE;
-    }
+	public static void copyStream(InputStream input, OutputStream output) throws IOException {
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
+	}
+
+	public static Blob createBlobFromFile(Blob blob, File file) {
+
+		try {
+			OutputStream outStream = blob.setBinaryStream(0);
+			FileInputStream blobStream = new FileInputStream(file);
+
+			try {
+
+				copyStream(blobStream, outStream);
+
+			} finally {
+				blobStream.close();
+				outStream.close();
+			}
+
+			return blob;
+		} catch (SQLException e) {
+			throw new EJApplicationException(e);
+		} catch (IOException e) {
+			throw new EJApplicationException(e);
+		}
+	}
 
 }
--- org.entirej.ora.gen/src/main/java/org/entirej/report/EJStatementParameterBlob.java
+++ org.entirej.ora.gen/src/main/java/org/entirej/report/EJStatementParameterBlob.java
@@ -1,25 +1,37 @@
 package org.entirej.report;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.sql.Types;
 
-import org.entirej.framework.report.service.EJReportParameterType;
+import org.entirej.framework.core.EJApplicationException;
+import org.entirej.framework.core.service.EJParameterType;
 import org.entirej.framework.report.service.EJReportStatementParameter;
 
 public class EJStatementParameterBlob extends EJReportStatementParameter
 {
     private static final int JDBC_TYPE = Types.BLOB;
 
-    public EJStatementParameterBlob(EJReportParameterType type)
+    public EJStatementParameterBlob(EJParameterType type)
     {
-        this(type, null);
+        super(type);
     }
 
-    public EJStatementParameterBlob(EJReportParameterType type, byte[] value)
+    public EJStatementParameterBlob(EJParameterType type, byte[] value)
     {
         super(type);
         setValue(value);
+    }
+    
+    public EJStatementParameterBlob(EJParameterType type, File value)
+    {
+    	super(type);
+    	setValue(value);
     }
 
   
@@ -29,4 +41,35 @@ public class EJStatementParameterBlob extends EJReportStatementParameter
         return JDBC_TYPE;
     }
 
+    
+    public static void copyStream(InputStream input, OutputStream output) throws IOException {
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
+	}
+
+	public static Blob createBlobFromFile(Blob blob, File file) {
+
+		try {
+			OutputStream outStream = blob.setBinaryStream(0);
+			FileInputStream blobStream = new FileInputStream(file);
+
+			try {
+
+				copyStream(blobStream, outStream);
+
+			} finally {
+				blobStream.close();
+				outStream.close();
+			}
+
+			return blob;
+		} catch (SQLException e) {
+			throw new EJApplicationException(e);
+		} catch (IOException e) {
+			throw new EJApplicationException(e);
+		}
+	}
 }
